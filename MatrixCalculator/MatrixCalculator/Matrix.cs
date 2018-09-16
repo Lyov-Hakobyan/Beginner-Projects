f5c57c86fbc9feab7050761b776d7af0e0167e39
Levon <43141834+Lyov-Hakobyan@users.noreply.github.com>
2018-09-17 01:28:05 +0400

    Add files via upload

---
+++ MatrixCalculator/MatrixCalculator/Matrix.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MatrixCalculator
+{
+
+
+    public class Matrix
+    {
+        private int Size1 { get; }
+        private int Size2 { get; }
+        private int[,] matrix;
+        static Random rd = new Random();
+        private bool CanPrint;
+
+        public Matrix(int size1, int size2)
+        {
+            this.Size1 = size1;
+            this.Size2 = size2;
+            matrix = new int[Size1, Size2];
+            CanPrint = false;
+            MakeMatrix();
+        }
+
+        private int[,] MakeMatrix()
+        {
+            for (int i = 0; i < Size1; i++)
+            {
+                for (int j = 0; j < Size2; j++)
+                {
+                    matrix[i, j] = rd.Next(-99, 100);
+                }
+            }
+            CanPrint = true;
+            return matrix;
+        }
+        public static void MatrixPrint(Matrix anyMatrix)
+        {
+            Console.WriteLine("---------------------------------------------");
+            if (anyMatrix.CanPrint == true)
+            {
+                for (int i = 0; i < anyMatrix.Size1; i++)
+                {
+                    for (int j = 0; j < anyMatrix.Size2; j++)
+                    {
+                        Write(anyMatrix.matrix[i, j] + " ");
+                    }
+                    WriteLine();
+                }
+                Write("\n");
+                anyMatrix.CanPrint = false;
+            }
+            Console.WriteLine("---------------------------------------------");
+        }
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            WriteLine("----------------First * Second---------------\n");
+            Matrix multiply = new Matrix(matrix1.Size1, matrix2.Size2);
+            if (matrix1.Size2 == matrix2.Size1)
+            {
+                for (int i = 0; i < matrix1.Size1; i++)
+                {
+                    for (int j = 0; j < matrix2.Size2; j++)
+                    {
+                        for (int k = 0; k < matrix1.Size2; k++)
+                        {
+                            multiply.matrix[i, j] += matrix1.matrix[i, k] * matrix2.matrix[k, j];
+                        }
+
+                    }
+                }
+                multiply.CanPrint = true;
+                return multiply;
+            }
+            else
+            {
+                multiply.CanPrint = false;
+                Console.WriteLine("Cann't do this operation...( * )\n");
+                return multiply;
+            }
+        }
+        public static Matrix operator *(Matrix matrix1, int number)
+        {
+            WriteLine("-----------------Matrix *  5-----------------\n");
+            Matrix multiply = new Matrix(matrix1.Size1, matrix1.Size2);
+            for (int i = 0; i < matrix1.Size1; i++)
+            {
+                for (int j = 0; j < matrix1.Size2; j++)
+                {
+                    multiply.matrix[i, j] = matrix1.matrix[i, j] * number;
+                }
+            }
+            multiply.CanPrint = true;
+            return multiply;
+        }
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            WriteLine("---------------First + Second----------------\n");
+            Matrix add = new Matrix(matrix1.Size1, matrix1.Size2);
+            if (matrix1.Size1 == matrix2.Size1 && matrix1.Size2 == matrix2.Size2)
+            {
+                for (int i = 0; i < matrix1.Size1; i++)
+                {
+                    for (int j = 0; j < matrix1.Size2; j++)
+                    {
+                        add.matrix[i, j] = matrix1.matrix[i, j] + matrix2.matrix[i, j];
+                    }
+                }
+                add.CanPrint = true;
+                return add;
+            }
+            else
+            {
+                Console.WriteLine("Can't do this operation...( + )\n");
+                add.CanPrint = false;
+                return add;
+            }
+
+        }
+        public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+        {
+            WriteLine("---------------First - Second----------------\n");
+            Matrix minus = new Matrix(matrix1.Size1, matrix1.Size2);
+            if (matrix1.Size1 == matrix2.Size1 && matrix1.Size2 == matrix2.Size2)
+            {
+                for (int i = 0; i < matrix1.Size1; i++)
+                {
+                    for (int j = 0; j < matrix1.Size2; j++)
+                    {
+                        minus.matrix[i, j] = matrix1.matrix[i, j] - matrix2.matrix[i, j];
+                    }
+                }
+                minus.CanPrint = true;
+                return minus;
+            }
+            else
+            {
+                Console.WriteLine("Can't do this operation...( - )\n");
+                minus.CanPrint = false;
+                return minus;
+            }
+        }
+    }
+}
---
+++ MatrixCalculator/MatrixCalculator/Program.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MatrixCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Clear();
+                Console.WriteLine("Enter sizes of first matrix\n");
+                int size1_1 = 0;
+                while (true)
+                {
+                    try
+                    {
+                        size1_1 = int.Parse(ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Wrong Input");
+                    }
+                }
+                int size1_2 = 0;
+                while (true)
+                {
+                    try
+                    {
+                        size1_2 = int.Parse(ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Wrong Input");
+                    }
+                }
+                Console.WriteLine("Enter sizes of second matrix\n");
+                int size2_1 = 0;
+                while (true)
+                {
+                    try
+                    {
+                        size2_1 = int.Parse(ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Wrong Input");
+                    }
+                }
+                int size2_2 = 0;
+                while (true)
+                {
+                    try
+                    {
+                        size2_2 = int.Parse(ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Wrong Input");
+                    }
+                }
+                Matrix matrix1 = new Matrix(size1_1, size1_2);
+                Matrix.MatrixPrint(matrix1);
+                Matrix matrix2 = new Matrix(size2_1, size2_2);
+                Matrix.MatrixPrint(matrix2);
+                Matrix.MatrixPrint(matrix1 * 5);
+                Matrix.MatrixPrint(matrix1 * matrix2);
+                Matrix.MatrixPrint(matrix1 - matrix2);
+                Matrix.MatrixPrint(matrix1 + matrix2);
+                WriteLine("Press Enter if you want do this again");
+            } while (ReadKey().Key == ConsoleKey.Enter);
+        }
+    }
+}
